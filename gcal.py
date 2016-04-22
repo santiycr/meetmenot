9741c2cce8b87285414961a35972371370dd6b97
Santiago Suarez Ordoñez <santiycr@gmail.com>
2016-04-21 17:58:29 -0700

    moving auth files to local dir

--- gcal.py
+++ gcal.py
@@ -12,8 +12,8 @@ import datetime
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/calendar-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+CLIENT_SECRET_FILE = '.secret.json'
+APPLICATION_NAME = 'MeetMeNot Robot'
 
 
 def get_credentials():
@@ -25,37 +25,27 @@ def get_credentials():
     Returns:
         Credentials, the obtained credential.
     """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'calendar-python-quickstart.json')
+    local_path = os.path.dirname(os.path.realpath(__file__))
+    credential_path = os.path.join(local_path, '.auth.json')
 
     store = oauth2client.file.Storage(credential_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
-        credentials = tools.run(flow, store)
-        # print('Storing credentials to ' + credential_path)
+        credentials = tools.run_flow(flow, store, None)
     return credentials
 
 
-def list_events(email, timeMin, timeMax):
-    """Shows basic usage of the Google Calendar API.
-
-    Creates a Google Calendar API service object and outputs a list of the next
-    10 events on the user's calendar.
-    """
+def list_events(calendar, timeMin, timeMax):
+    """ pull event for a user calendar based on a start and end time """
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
 
-    # print('Getting the upcoming 10 events')
     eventsResult = service.events().list(
-        calendarId=email, timeMin=timeMin, timeMax=timeMax, singleEvents=True,
-        orderBy='startTime').execute()
+        calendarId=calendar, timeMin=timeMin, timeMax=timeMax,
+        singleEvents=True, orderBy='startTime').execute()
     return eventsResult.get('items', [])
 
 
@@ -66,7 +56,7 @@ if __name__ == '__main__':
                          now.isoformat() + 'Z',
                          (now + full_day).isoformat() + 'Z')
     if not events:
-        print('No upcoming events found.')
+        print('No events found.')
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(event)
