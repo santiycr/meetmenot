97a641053f523e182cece2a870ead91bfcb55cb0
Santiago Suarez Ordoñez <santiycr@gmail.com>
2016-04-21 17:00:43 -0700

    docstring for the basic validation example

--- heuristics/basic.py
+++ heuristics/basic.py
@@ -3,6 +3,12 @@
 
 def validate(event):
     """
-    Validate basic description size
+    Basic validation heuristic example
+
+    all heuristic validations will be called with an event
+    and are expected to return a tuple:
+      - bool: indicating whether the event is invalid
+      - message: if the event is invalid, includes an error message to be used
+        for the notification
     """
     return False, None
